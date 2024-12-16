fb038c822192a4f97622f9fe84235933b60db401
genie <soccer910@naver.com>
2024-12-16 21:21:55 +0900

    app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,41 @@
-export default function App() {}
+import Header from "./components/Header.js";
+import RegionList from "./components/RegionList.js";
+import CityList from "./components/CityList.js";
+import CityDetail from "./components/CityDetail.js";
+
+import { request } from "./components/api.js";
+
+export default function App($app) {
+  this.state = {
+    startIdx: 0,
+    sortBy: "",
+    searchWord: "",
+    region: "",
+    cities: "",
+  };
+
+  const header = new Header();
+  const regionList = new RegionList();
+  const cityList = new CityList({ $app, initialState: this.state.cities });
+  const cityDetail = new CityDetail();
+
+  this.setState = (newState) => {
+    this.state = newState;
+    cityList.setState(this.state.cities);
+  };
+
+  const init = async () => {
+    const cities = await request(
+      this.state.startIdx,
+      this.state.region,
+      this.state.sortBy,
+      this.state.searchWord
+    );
+    this.setState({
+      ...this.state,
+      cities: cities,
+    });
+  };
+
+  init();
+}
---
+++ src/components/CityList.js
@@ -0,0 +1,34 @@
+export default function CityList($app, initialState) {
+  this.state = initialState;
+  this.$target = document.createElement("div");
+  this.$target.className = "city-list";
+
+  $app.appendChild(this.$target);
+
+  this.template = () => {
+    let temp = `<div class="city-items-container"></div>`;
+    if (this.state) {
+      this.state.cities.forEach((elm) => {
+        temp += `
+        <div class = "city-item" id = ${elm.id}>
+          <img src=${elm.image}></img>
+          <div class="city-item-info">${elm.city}, ${elm.country}</div>
+          <div class="city-item-score">★ ${elm.total}</div>
+        </div>
+        `;
+      });
+      temp += `</div>`;
+    }
+    return temp;
+  };
+
+  this.render = () => {
+    this.$target.innerHTML = this.template();
+  };
+
+  this.setState = (setState) => {
+    this.state = newState;
+    this.render();
+  };
+  this.render();
+}
